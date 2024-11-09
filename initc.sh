6df9415d804d4ad8aebba85c5b32e826bc2e3bd3
Oskari Lappalainen <oskari.j.lappalainen@gmail.com>
2024-11-09 08:36:52 +0200

    changes

--- initc.sh
+++ initc.sh
@@ -1,69 +1,18 @@
 #!/bin/bash
 
+RESOURCE_DIR="resources"
+PROJECT_DIR=$1
+LANGUAGE=$2
+echo $RESOURCE_DIR
 
-# FUNCTION DEFINITIONS
-function handle_error() {
-    ERROR_MESSAGE=$1
+mkdir $PROJECT_DIR
+cd $PROJECT_DIR
 
-    echo ${ERROR_MESSAGE}
-    echo "Exiting..."
-    exit 1
-}
-
-
-function log_folder_creation() {
-    RELATIVE_FOLDER_PATH=$1
-
-    echo "Created folder $(pwd)/$1"
-}
-
-
-function log_file_creation() {
-    RELATIVE_FILE_PATH=$1
-
-    echo "Created file $(pwd)/$1"
-}
-
-
-# CONSTANT DEFINITIONS
-USER_NAME=$(whoami)
-PROJECT_NAME="$1"
-SUBFOLDERS=("bin" "build" "include" "src")
-MAKEFILE_PATH="/home/${USER_NAME}/scripts/initc/resources/makefile"
-MAIN_FILE_PATH="/home/${USER_NAME}/scripts/initc/resources/main.c"
-
-
-PROJECT_DIRECTORY_ERROR="Error: directory '${PROJECT_NAME}' already exists."
-MAKEFILE_ERROR="Error: file '${MAKEFILE_PATH}' not found."
-MAIN_FILE_ERROR="Error: file '${MAIN_FILE_PATH}' not found."
-
-
-# Test for errors.
-if test -d "$PROJECT_NAME"; then
-    handle_error "$PROJECT_DIRECTORY_ERROR"
-elif ! test -f $MAKEFILE_PATH; then
-    handle_error "$MAKEFILE_ERROR"
-elif ! test -f $MAIN_FILE_PATH; then
-    handle_error "$MAIN_FILE_ERROR"
-fi
-
-
-mkdir "$PROJECT_NAME"
-log_folder_creation "$PROJECT_NAME"
-cd "$PROJECT_NAME"
-echo "changed pwd to $(pwd)"
-
-
-for folder in ${SUBFOLDERS[@]}; do
-    mkdir $folder
-    log_folder_creation $folder
-done
-
-
-cp $MAKEFILE_PATH .
-log_file_creation makefile
-cp $MAIN_FILE_PATH src
-log_file_creation main.c
-
-echo "Project creation successful!"
-
+case ${LANGUAGE} in
+    C)
+        cp ${RESOURCE_DIR}/* $PROJECT_DIR
+        ;;
+    *)
+        echo "Unknown language" $LANGUAGE
+        ;;
+esac
