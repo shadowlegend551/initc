e35424a341ac70361dbbe28c69871839b7fd696e
shadowlegend551 <oskari.j.lappalainen@gmail.com>
2024-12-23 23:47:05 +0200

    reworked the project

--- install.sh
+++ install.sh
@@ -2,17 +2,14 @@
 
 BIN_DIR="$HOME/.local/bin"
 AUX_DIR="$HOME/.local/share/initc"
-CONFIG_DIR="$HOME/.config/initc"
 
 echo "Creating missing folders..."
 mkdir -p ${BIN_DIR}
 mkdir -p ${AUX_DIR}
-mkdir -p ${CONFIG_DIR}
 
 echo "Copying files over..."
 cp src/initc ${BIN_DIR}
-cp resources/* ${AUX_DIR}
-cp config/initc.conf ${CONFIG_DIR}
+cp -r resources/presets/* ${AUX_DIR}
 
 echo "Installation finished!"
 
---
+++ resources/presets/c/src/main.c
@@ -0,0 +1,13 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+
+int main()
+{
+    printf("Hello World!\n");
+    return 0;
+}
+
