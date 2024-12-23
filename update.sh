0f2156e71ecf82ab2a73b7be2688d30222c01ea3
shadowlegend551 <oskari.j.lappalainen@gmail.com>
2024-12-23 23:57:36 +0200

    Added logging to update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+echo "Backing up custom languages..."
 mv "${HOME}/.local/share/initc/custom" .
+
+echo "Pulling newest commit..."
 git pull git@github.com:shadowlegend551/initc.git
+
+echo "Installing the newest commit..."
 ./install.sh
+
+echo "Restoring custom languages..."
 mv "custom" "${HOME}/.local/share/initc"
+
+echo "Finished updating!"
 
